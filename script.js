1bbdee9c79b231b3174b7e27f80510572d988a82
Jayed Hossain <jayedhossain.doict@gmail.com>
2023-07-25 17:34:17 +0600

    First commit

---
+++ script.js
@@ -0,0 +1,11 @@
+tailwind.config = {
+    theme: {
+      extend: {
+        colors: {
+          clifford: '#da373d',
+          'gradient-start': '#F00',
+          'gradient-end': '#FF8938',
+        }
+      }
+    }
+  }
